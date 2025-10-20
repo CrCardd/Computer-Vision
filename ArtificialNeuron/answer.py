d84252ce99a9f2d109858d31fead3e53963bdad9
CrCardd <cris.rc377@gmail.com>
2025-10-20 06:46:00 -0300

    ...

--- ArtificialNeuron/answer.py
+++ ArtificialNeuron/answer.py
@@ -1,24 +1,21 @@
 from random import random, randint
 from math import exp, sqrt
 from matplotlib import pyplot as plt
-
+ 
 class NeuralNet:
   def __init__(self):
     self.layers = []
-	
-	# Primeira Camada
+ 
     w = [[random() for i in range(4)] for j in range(2)]
     b = [random() for i in range(2)]
     self.layers.append((w, b))
     
-	# Segunda Camada
     w = [[random() for i in range(2)] for j in range(3)]
-    # TODO  
+    b = [random() for i in range(3)]
     self.layers.append((w, b))
   
-  def sigmoid(self, x): 
-    # TODO
-    pass
+  def sigmoid(self, x):
+    return 1 / (1 + exp(-x))
   
   def predict(self, X):
     y = X
@@ -26,20 +23,18 @@ class NeuralNet:
       w, b = layer
       output = []
       for wj, bj in zip(w, b):
-    	# TODO
-    	# TODO 
-        pass
-      
-      y = output # passando pra próxima layer
+        s = sum([wi * yi for wi, yi in zip(wj, y)]) + bj
+        output.append(self.sigmoid(s))
+      y = output
     return y
   
   def transformY(self, y):
     if y == 0:
       return [1, 0, 0]
-    # TODO
-    # TODO
-    # TODO
-    # TODO
+    elif y == 1:
+      return [0, 1, 0]
+ 
+    return [0, 0, 1]
   
   def score(self, X, Y):
     Y = self.transformY(Y)
@@ -57,54 +52,54 @@ class NeuralNet:
     scores = []
     for i in range(epochs):
       print("iniciando epoca", i + 1)
-      # TODO
-
+      self.epoch(X, Y)
+ 
       score = self.scoreAll(X, Y)
       scores.append(score)
       print("score", score)
       print()
     return scores
-
+ 
   def epoch(self, X, Y):
-    # TODO
+    batch = randint(0, len(Y) - 1)
     x = X[batch]
     y = Y[batch]
-	
+ 
     y = self.transformY(y)
     pred = self.predict(x)
-
+ 
     helper = []
+ 
     w, b = self.layers[1]
     for j in range(len(b)):
       yj = pred[j]
-      # TODO 
+      dB = (yj - y[j]) * yj * (1 - yj)
       b[j] -= 0.25 * dB
       for i in range(len(w[j])):
         helper.append(dB * w[j][i] * x[i] * w[j][i])
-      	# TODO  
+        w[j][i] -= 0.25 * dB * w[j][i] * x[i]
     self.layers[1] = (w, b)
-
+ 
     w, b = self.layers[0]
     for j in range(len(b)):
       dB = 0
       yj = self.sigmoid(sum([wj * xj for wj, xj in zip(w[j], x)]) + b[j])
       for lastDevs in helper:
-        dB += helper[j] * yj
-      # TODO
+        dB += lastDevs * yj
+      b[j] -= 0.25 * dB
       for i in range(len(w[j])):
         w[j][i] -= 0.25 * dB * w[j][i] * x[i]
     self.layers[0] = (w, b)
+ 
   
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-
+ 
 X, Y = load_iris(return_X_y = True)
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.15, random_state=42)
-
-# TODO
-mode = 
+ 
+model = NeuralNet()
 scores = model.fit(X_train, Y_train)
-model.scoreAll(X_test, Y_test)
-
-print(scores)
+a = model.scoreAll(X_test, Y_test)
+print(a)
 plt.plot(scores)
--- ArtificialNeuron/network.py
+++ ArtificialNeuron/network.py
@@ -1,21 +1,27 @@
 from typing import List, Tuple
-from random import random
+from random import random, randint
 import math
 
 class Network:
     def __init__(self):
         self.layers : List[Tuple[List[List[float]],float]] = []
+        self.lr = 0.1
 
         n = 4 # quantidade de features do dataset
 
-        nL1 = 3 #quantidade de neuronios da primeira camada
+        nL1 = 2 # quantidade de neuronios da primeira camada
         w = [[random() for i in range(n)] for _ in range(nL1)]
         b = [random() for _ in range(nL1)]
         self.layers.append((w,b))
 
-        nL2 = 3
-        w = [[random() for i in range(nL1)] for _ in range(nL2)]
-        b = [random() for _ in range(nL2)]
+        # nL2 = 6
+        # w = [[random() for i in range(nL1)] for _ in range(nL2)]
+        # b = [random() for _ in range(nL2)]
+        # self.layers.append((w,b))
+       
+        nL3 = 3
+        w = [[random() for i in range(nL1)] for _ in range(nL3)]
+        b = [random() for _ in range(nL3)]
         self.layers.append((w,b))
 
     def sigmoid(self, s : float):
@@ -45,18 +51,77 @@ class Network:
         a[y] = 1
         return a
 
-    def score(self):
-        pass
-    
-    def scoreAll(self):
-        pass
+    def epoch(self, x : List[float], y : float):
+        # Forwardpass
+        x_for = [x[:]]
+        x_values = x[:]
+        for layer in self.layers:
+            results = []
+            w, b = layer
+            for i in range(len(w)):
+                s = sum(w[i][j]*x_values[j] for j in range(len(x_values))) + b[i]
+                results.append(self.sigmoid(s))
+            x_values = results[:]
+            x_for.append(x_values)
 
-    def fit(self):
-        pass
+        y : List[float] = self.transformY(y)
+        output_error = []
 
-    def epoch(self):
-        pass
+        # Output layer
+        w, b = self.layers[-1]
+        for j in range(len(w)):
+            p = x_for[-1][j]
+
+            delta = (p-y[j]) * p*(1-p)
+            output_error.append(delta)
+
+            b[j] = b[j] - self.lr * (delta)
+            for k in range(len(w[j])):
+                w[j][k] = w[j][k] - self.lr * (delta * x_for[-2][k])
+        self.layers[-1] = (w,b)
+
+        # Hidden layers
+        for i in range(len(self.layers)-2, -1, -1):
+            w, b = self.layers[i]
+            next_ws, _ = self.layers[i+1]
+            crr_layer_error = []
+            for j in range(len(b)):
+                p = x_for[i+1][j]
+                
+                w_error  = sum(next_ws[k][j] * output_error[k] for k in range(len(output_error)))
+                delta = p * (1 - p) * w_error
+                crr_layer_error.append(delta)
+                
+                b[j] = b[j] - self.lr * delta
+                for k in range(len(w[j])):
+                    w[j][k] = w[j][k] - self.lr * (delta * x_for[i][k])
+            output_error = crr_layer_error[:]
+            self.layers[i] = (w,b)
+       
+    def score(self, x : List[float], y : float):
+        r : List[float] = self.transformY(y)
+        y : List[float] = self.predict(x)
+        score = sum((ri-yi)**2 for ri, yi in zip(r,y))
+
+        return score**0.5
     
+    def scoreAll(self, x : List[List[float]], y : List[float]):
+
+        score = 0
+        for xi, yi in zip(x,y):
+            score += self.score(xi, yi)
+        return score / len(y)
+
+    def fit(self, x : List[List[float]], y = List[float], epochs : int = 100):
+
+        
+        for i in range(epochs):
+            print(f'starting epoch\t [{i+1}]')
+            for xj, yj in zip(x,y):
+                self.epoch(xj,yj)
+            # self.epoch(x[randint(0,len(y)-1)], y[randint(0,len(y))-1])
+            print('score:\t\t', self.scoreAll(x,y))
+            print()
 
 
 
@@ -68,10 +133,7 @@ from sklearn.model_selection import train_test_split
 X, Y = load_iris(return_X_y = True)
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.15, random_state=42)
 
-
-
-
-
-print(X_train)
-print()
-print(Y_train)
+Net = Network()
+Net.fit(X_train, Y_train,1)
+a = Net.scoreAll(X_test, Y_test)
+print(a)
