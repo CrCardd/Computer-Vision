9c4d38f5de3a830de9fd6178343846f4bb4db1f6
CrCardd <cris.rc377@gmail.com>
2025-10-24 07:34:49 -0300

    ...

---
+++ ArtificialNeuron/network.py
@@ -0,0 +1,138 @@
+from typing import List, Tuple
+from random import random, randint
+import math
+
+class Network:
+    def __init__(self):
+        self.layers : List[Tuple[List[List[float]],float]] = []
+        self.lr = 0.1
+
+        n = 4 # quantidade de features do dataset
+
+        nL1 = 2 # quantidade de neuronios da primeira camada
+        w = [[random() for i in range(n)] for _ in range(nL1)]
+        b = [random() for _ in range(nL1)]
+        self.layers.append((w,b))
+
+        # nL2 = 6
+        # w = [[random() for i in range(nL1)] for _ in range(nL2)]
+        # b = [random() for _ in range(nL2)]
+        # self.layers.append((w,b))
+       
+        nL3 = 3
+        w = [[random() for i in range(nL1)] for _ in range(nL3)]
+        b = [random() for _ in range(nL3)]
+        self.layers.append((w,b))
+
+    def sigmoid(self, s : float):
+        return 1/(1 + math.e**-s)
+    
+    def predict(self, x : List[float]):
+
+        for layer in self.layers:
+            w, b = layer
+            y_results = []        
+            for wj, bj in zip(w,b):
+                s : float = 0
+                for i in range(len(x)):
+                    s += x[i] * wj[i]
+                s += bj
+                y = self.sigmoid(s)
+                y_results.append(y)
+
+            x = y_results
+
+        return y_results
+
+    def transformY(self, y : int):
+        a = [0, 0, 0]
+        if(y >= len(a)):
+            return a
+        a[y] = 1
+        return a
+
+    def epoch(self, x : List[float], y : float):
+        # Forwardpass
+        x_for = [x[:]]
+        x_values = x[:]
+        for layer in self.layers:
+            results = []
+            w, b = layer
+            for i in range(len(w)):
+                s = sum(w[i][j]*x_values[j] for j in range(len(x_values))) + b[i]
+                results.append(self.sigmoid(s))
+            x_values = results[:]
+            x_for.append(x_values)
+
+        y : List[float] = self.transformY(y)
+        output_error = []
+
+        # Output layer
+        w, b = self.layers[-1]
+        for j in range(len(w)):
+            p = x_for[-1][j]
+
+            delta = (p-y[j]) * p*(1-p)
+            output_error.append(delta)
+
+            b[j] = b[j] - self.lr * (delta)
+            for k in range(len(w[j])):
+                w[j][k] = w[j][k] - self.lr * (delta * x_for[-2][k])
+        self.layers[-1] = (w,b)
+
+        # Hidden layers
+        for i in range(len(self.layers)-2, -1, -1):
+            w, b = self.layers[i]
+            next_ws, _ = self.layers[i+1]
+            crr_layer_error = []
+            for j in range(len(b)):
+                p = x_for[i+1][j]
+                
+                w_error  = sum(next_ws[k][j] * output_error[k] for k in range(len(output_error)))
+                delta = p * (1 - p) * w_error
+                crr_layer_error.append(delta)
+                
+                b[j] = b[j] - self.lr * delta
+                for k in range(len(w[j])):
+                    w[j][k] = w[j][k] - self.lr * (delta * x_for[i][k])
+            output_error = crr_layer_error[:]
+            self.layers[i] = (w,b)
+       
+    def score(self, x : List[float], y : float):
+        r : List[float] = self.transformY(y)
+        y : List[float] = self.predict(x)
+        score = sum((ri-yi)**2 for ri, yi in zip(r,y))
+
+        return score**0.5
+    
+    def scoreAll(self, x : List[List[float]], y : List[float]):
+
+        score = 0
+        for xi, yi in zip(x,y):
+            score += self.score(xi, yi)
+        return score / len(y)
+
+    def fit(self, x : List[List[float]], y = List[float], epochs : int = 100):
+        
+        for i in range(epochs):
+            print(f'starting epoch\t [{i+1}]')
+            for xj, yj in zip(x,y):
+                self.epoch(xj,yj)
+            # self.epoch(x[randint(0,len(y)-1)], y[randint(0,len(y))-1])
+            print('score:\t\t', self.scoreAll(x,y))
+            print()
+
+
+
+
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+X, Y = load_iris(return_X_y = True)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.15, random_state=42)
+
+Net = Network()
+Net.fit(X_train, Y_train, 1, 28)
+a = Net.scoreAll(X_test, Y_test)
+print(a)
--- Network/to_draw_network.py
+++ Network/to_draw_network.py
@@ -3,6 +3,8 @@ from typing import List
 import numpy as np
 import math
 
+
+
 BACKGROUNG_COLOR                = "#111111"
 
 NEURON_DISABLE_COLOR            = "#424242"
@@ -20,12 +22,14 @@ LINE_COLOR      = "#802A67"
 ERROR_COLOR     = "#BD3636"
 
 # --------------------- DELAY -----------------------
-DELAY = 10
+DELAY = 0
 # ---------------------------------------------------
 
 
 # ------------------ NEURON SIZE --------------------
 CIRCLE_RADIUS = 18
+# -----------------  WEIGHT SIZE --------------------
+WEIGHT_SIZE = 12
 # ---------------------------------------------------
 
 
@@ -35,13 +39,15 @@ NEURONS_MARGIN_X = CIRCLE_RADIUS*1.5
 root = tk.Tk()
 root.title("Neural Network")
 root.attributes('-fullscreen', True)
+root.bind("<Escape>", lambda e: root.destroy())
+
 SCREEN_W = root.winfo_screenwidth()
 SCREEN_H = root.winfo_screenheight()
 
 MAX_NEURONS = int(SCREEN_H / (CIRCLE_RADIUS*2+NEURONS_MARGIN_Y))
 MAX_NEURONS = MAX_NEURONS - (1 if MAX_NEURONS % 2 == 0 else 0)
-print(MAX_NEURONS)
 
+MAX_WEIGHTS = int(SCREEN_W/4/WEIGHT_SIZE)
 
 
 canvas = tk.Canvas(root, width=SCREEN_W, height=SCREEN_H, bg=BACKGROUNG_COLOR)
@@ -85,7 +91,7 @@ class Neuron:
     def active(self, s : float):
         return 1/(1 + math.e**-s)
 
-    def fit(self, x, t=None, delta = None):
+    def fit(self, x, t=None, delta = None, ids_matrix=[], scaler=100, draw = False):
         if(delta is None):
             y = self.predict(x)
             delta = (y-t) * y*(1-y)
@@ -94,16 +100,37 @@ class Neuron:
         for i in range(len(self.w)):
             self.w[i] = self.w[i] - self.lr * (delta * x[i])
         
+
+        if draw:
+            for i in range(len(x)):
+                l = int(i/len(ids_matrix[0]))
+                c = i%len(ids_matrix[0])
+                id = ids_matrix[l][c]
+                
+                w_min = np.min(self.w)
+                w_max = np.max(self.w)
+
+                intensity = int((self.w[i]-w_min)/(w_max-w_min) * scaler)
+                canvas.itemconfig(
+                    id, 
+                    fill = f"#{intensity if self.w[i] < 0 else 0:02x}{intensity if self.w[i] > 0 else 0:02x}{0:02x}"
+                )
+            canvas.update()
+        
         return delta
 
 class Network:
-    def __init__(self, outputs, features):
+    def __init__(self, outputs, features, neuron_layers = [], line_limit = None, scaler = 100):
         self.layers : List[List[Neuron]] = []
         self.lr = 0.1
         self.outputs = outputs
         self.features = features # quantidade de features do dataset
+        self.line_limit = line_limit
+        self.scaler = scaler
+        self.w_matrix = []
+        self.x_matrix = []
 
-        nLs = [5,8,3,5] # Neuronios por camada
+        nLs = neuron_layers # Neuronios por camada
 
         nLs.append(self.outputs)
 
@@ -123,7 +150,6 @@ class Network:
                     fill=MULTI_NEURON_DISABLE_COLOR,
                     outline="#dedede"
                 )
-                canvas.update()
 
             m_neurons = min(MAX_NEURONS, nLs[i])
             y_mid = (SCREEN_H/2 - ((m_neurons-1)*(CIRCLE_RADIUS*2+NEURONS_MARGIN_Y))/2)
@@ -162,7 +188,8 @@ class Network:
 
             nO = nLs[i]
             self.layers.append(neurons)
-         
+        canvas.update()
+          
     def predict(self, x : List[float]):
 
         for layer, i in zip(self.layers, range(len(self.layers))):
@@ -189,6 +216,17 @@ class Network:
         return a
 
     def epoch(self, x : List[float], y : float):
+        # X_Matrix
+        for i in range(len(x)):
+            l = int(i/self.line_limit)
+            c = i%self.line_limit
+            id = self.x_matrix[l][c]
+            intensity = int(x[i] / self.scaler * self.scaler)
+            canvas.itemconfig(
+                id, 
+                fill = f"#{intensity:02x}{intensity:02x}{intensity:02x}"
+            )
+
         # Forwardpass
         x_for = [x[:]]
         x_values = x[:]
@@ -199,7 +237,7 @@ class Network:
                 p = neuron.predict(x_values)
                 self.set_color_neuron(neuron, neuron.enable_color, neuron.outline_enable_color)
                 if(i < len(self.layers)-1):
-                    self.connect_next_layer(neuron, self.layers[i+1], LINE_COLOR)                    
+                    self.connect_next_layer(neuron, self.layers[i+1], LINE_COLOR)            
                 self.set_color_neuron(neuron, neuron.disable_color, neuron.outline_disable_color)
                 results.append(p)
 
@@ -212,7 +250,7 @@ class Network:
         # Output layer
         output_layer = self.layers[-1]
         for i in range(len(output_layer)):
-            delta = output_layer[i].fit(x_for[-2], targets[i])
+            delta = output_layer[i].fit(x_for[-2], targets[i], ids_matrix=self.w_matrix, scaler=self.scaler, draw=len(self.layers) == 1)
             output_error.append(delta)
 
         # Hidden layers
@@ -230,10 +268,20 @@ class Network:
                 self.set_color_neuron(neuron, neuron.disable_color, neuron.outline_disable_color)
                 
                 delta = p * (1 - p) * w_error
-                self.layers[i][j].fit(x_for[i], delta=delta)
+                self.layers[i][j].fit(x_for[i], delta=delta, ids_matrix=self.w_matrix, scaler=self.scaler, draw = i == 0)
                 crr_layer_error.append(delta)
 
             output_error = crr_layer_error[:]
+        
+        for i in range(len(x)):
+            l = int(i/len(self.w_matrix[0]))
+            c = i%len(self.w_matrix[0])
+            id = self.w_matrix[l][c]
+
+            canvas.itemconfig(
+                id, 
+                fill = f"#{0:02x}{0:02x}{0:02x}"
+            )
        
     def score(self, x : List[float], y : float):
         r : List[float] = self.transformY(y)
@@ -250,6 +298,50 @@ class Network:
         return score / len(y)
 
     def fit(self, x : List[List[float]], y : List[float], epochs : int = 100):
+
+        if self.line_limit == None:
+            self.line_limit = MAX_WEIGHTS
+
+        self.line_limit = min(len(x[0]), self.line_limit, MAX_WEIGHTS)
+        total_lines = int(len(x[0])/self.line_limit)
+        mid_x = SCREEN_W - SCREEN_W/4
+        mid_y = SCREEN_H/4
+
+        x_init = mid_x - int(self.line_limit/2 * WEIGHT_SIZE)
+        y_init = mid_y - int(total_lines/2 * WEIGHT_SIZE)
+
+        for i in range(total_lines):
+
+            pos_y = y_init + i*WEIGHT_SIZE
+            w_line = []
+            x_line = []
+            for j in range(min(len(x[0])-i*self.line_limit, self.line_limit)):
+                pos_x = x_init + j*WEIGHT_SIZE
+
+                w_id = canvas.create_rectangle(
+                    pos_x - WEIGHT_SIZE/2,
+                    pos_y - WEIGHT_SIZE/2,
+                    pos_x + WEIGHT_SIZE/2,
+                    pos_y + WEIGHT_SIZE/2,
+                    fill="#000000", 
+                    outline="#717171"
+                )
+
+                x_id = canvas.create_rectangle(
+                    pos_x - WEIGHT_SIZE/2,
+                    pos_y - WEIGHT_SIZE/2 + SCREEN_H/2,
+                    pos_x + WEIGHT_SIZE/2,
+                    pos_y + WEIGHT_SIZE/2 + SCREEN_H/2,
+                    fill="#000000", 
+                    outline="#717171"
+                )
+                w_line.append(w_id)
+                x_line.append(x_id)
+
+            self.w_matrix.append(w_line)
+            self.x_matrix.append(x_line)
+
+
 
         
         for i in range(epochs):
@@ -293,23 +385,6 @@ class Network:
             line_ids.append(line_id)
 
         root.update()    
-        root.after(int(DELAY/2))
         for line_id in line_ids:
             canvas.delete(line_id)
-
         root.update()
-
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-
-X, Y = load_iris(return_X_y = True)
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.15, random_state=42)
-
-
-
-Net = Network(outputs=3, features=4)
-
-
-root.after(1000, lambda: Net.fit(X_train, Y_train, 1))
-
-root.mainloop()
