aa111e07afd5de219fbaff907282eb8f6960e87a
CrCardd <cris.rc377@gmail.com>
2025-10-31 17:30:39 -0300

    kernel

--- Kernel/main.py
+++ Kernel/main.py
@@ -1,15 +1,8 @@
 from typing import List
 from Kernel import Kernel, init
 
-import numpy as np
-from scipy.signal import convolve2d
-
 def filter_function(image: List[List[int]], kernel: List[List[int]]):
-    kernel = np.array(kernel)
-    image = np.array(image)
-    filtered = convolve2d(image, kernel, mode='same', boundary='fill', fillvalue=0)
-    filtered = np.clip(filtered, 0, 255).astype(np.uint8)
-    return filtered
+    return image[:]
 
 Kernel = Kernel("minion.jfif", filter_function)
 
